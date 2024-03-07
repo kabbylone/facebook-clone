2b2e96a90ff2cab8b4628ffb9110b752c7f313e2
Kabbier <90781243+kabbylone@users.noreply.github.com>
2024-03-07 16:08:49 +0530

    Header created

---
+++ src/Header.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import FlagIcon from "@mui/icons-material/Flag";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import { Avatar, IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import ForumIcon from "@mui/icons-material/Forum";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/900px-Facebook_f_logo_%282019%29.svg.png?20231203063624"
+          alt=""
+        />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__center">
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Kabbier</h4>
+        </div>
+
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
