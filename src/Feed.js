17c2ff6e5b2d07e93df8387f0b80bbd4a6838379
Kabbier <balinagkabbier@gmail.com>
2024-03-16 17:28:04 +0530

    Deployed on firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,42 +1,36 @@
 import "./css/Feed.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StoryReel from "./StoryReel.js";
 import MessageSender from "./MessageSender.js";
 import Post from "./Post.js";
+import db from "./firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+      );
+  }, []);
+
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
 
-      <Post
-        profilePic={`${process.env.PUBLIC_URL}/profile_pic.jpg`}
-        message={
-          "Adrenaline-fueled perfection. Leaning into corners at insane speeds, these MotoGP riders make the impossible look effortless. #MotoGP #SpeedDemons⚡🏍️"
-        }
-        timestamp={"Timestamp"}
-        username={"Kabbier Bali Nag"}
-        image={`${process.env.PUBLIC_URL}/moto_gp.png`}
-      />
-      <Post
-        profilePic={`${process.env.PUBLIC_URL}/profile_pic_4.jpg`}
-        message={
-          "Captivated by the timeless elegance of India Gate, a symbol of pride and patriotism"
-        }
-        timestamp={"Timestamp"}
-        username={"Andrea Piacquadio"}
-        image={`${process.env.PUBLIC_URL}/india_gate.jpg`}
-      />
-      <Post
-        profilePic={`${process.env.PUBLIC_URL}/profile_pic_1.jpg`}
-        message={
-          "Embracing innovation and collaboration at our corporate headquarters, where ideas meet excellence."
-        }
-        timestamp={"Timestamp"}
-        username={"Mi Ziao"}
-        image={`${process.env.PUBLIC_URL}/corporate-office.jpg`}
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.data.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+        />
+      ))}
     </div>
   );
 }
--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from "@mui/icons-material/Videocam";
 import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import firebase from "firebase/compat/app";
 
 function MessageSender() {
   const [{ user }, dispatch] = useStateValue();
@@ -13,6 +15,14 @@ function MessageSender() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    db.collection("posts").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      profilePic: user.photoURL,
+      username: user.displayName,
+      image: imageUrl,
+    });
 
     setInput("");
     setImageUrl("");
